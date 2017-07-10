f7db71f4cef139e2c45cc86340345f8ee538f3a5
付野 <834225691@qq.com>
2017-07-10 18:04:12 +0800

    first commit

--- tableview下拉放大/ViewController.m
+++ tableview下拉放大/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#define kHeight 250
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableview;
 
 @end
 
@@ -16,14 +18,45 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _tableview.delegate = self;
+    _tableview.dataSource = self;
+    [_tableview registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell_id"];
+    _tableview.contentInset = UIEdgeInsetsMake(kHeight, 0, 0, 0);
+    UIImageView *imageview = [[UIImageView alloc] initWithFrame:CGRectMake(0, -kHeight, self.view.frame.size.width, kHeight)];
+    imageview.image = [UIImage imageNamed:@"123.jpg"];
+    //图片宽高比一定要大于设定宽高比
+    imageview.contentMode = UIViewContentModeScaleAspectFill;
+    imageview.tag = 101;
+    [_tableview addSubview:imageview];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 30;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell_id" forIndexPath:indexPath];
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld--%ld", indexPath.section, indexPath.row];
+    return cell;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    CGPoint point = scrollView.contentOffset;
+    NSLog(@"%f", point.y);
+    if (point.y < -kHeight) {
+        CGRect rect = [_tableview viewWithTag:101].frame;
+        rect.origin.y = point.y;
+        rect.size.height = - point.y;
+        [_tableview viewWithTag:101].frame = rect;
+        NSLog(@"%f",rect.size.height);
+    }
+}
 
 @end
